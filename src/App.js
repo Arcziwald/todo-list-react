f635af05f480136b84bcb0ecca676a36af32d5ad
Artur Mochnia <amochnia@gmail.com>
2023-06-01 10:35:43 +0100

    update first change

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React from `react`;
+import Form from "./Form";
+
+const tasks = [
+  { id: 1, content: "przejść na Reacta", done: false },
+  { id: 2, content: "zjeść obiad", done: true },
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body className="body">
+    <main className="container">
+        <header className="header"> 
+            <h1 className="header__title">Lista zadań</h1>
+        </header>
+        <section className="section">
+            <h2 className="section__title">Dodaj nowe zadanie</h2>
+                <form className="form">
+                    <input className="form__input" placeholder="Co jest do zrobienia?" autofocus/>
+                    <button className="form__button">Dodaj zadanie</button>
+                </form>
+        </section>
+        <section className="section">
+        <header className="section__header">
+           <h2 className="section__title"> Lista zadań</h2>
+          <div className="stat__button">
+            <button className="stat__button js-toggleHideDoneButton">Ukryj ukończone</button>
+            <button className="stat__button js-markALLDoneButton">Ukończ wszystkie</button> 
+          </div>
+        </header>
+           <ul className="tasks"></ul>
+        </section>
+    </main>
+    </body>
+
   );
 }
 
---
+++ src/Form/index.js
@@ -0,0 +1,9 @@
+
+const Form = () => (
+    <form className="form">
+        <input className="form__input" placeholder="Co jest do zrobienia?" autofocus/>
+        <button className="form__button">Dodaj zadanie</button>
+    </form>
+)
+
+export default Form;
