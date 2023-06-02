84dd66952e85e1716c07ae7ae0bcfff98fe4a9ad
Artur Mochnia <amochnia@gmail.com>
2023-06-02 11:52:45 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,38 +1,36 @@
-import React from `react`;
+import React from "react";
 import Form from "./Form";
+import Tasks from "./Tasks";
+import Buttons from "./Buttons";
 
 const tasks = [
   { id: 1, content: "przejść na Reacta", done: false },
   { id: 2, content: "zjeść obiad", done: true },
 ];
 
+const hideDoneTasks = false;
+
 function App() {
   return (
     <body className="body">
-    <main className="container">
-        <header className="header"> 
-            <h1 className="header__title">Lista zadań</h1>
+      <main className="container">
+        <header className="header">
+          <h1 className="header__title">Lista zadań</h1>
         </header>
         <section className="section">
-            <h2 className="section__title">Dodaj nowe zadanie</h2>
-                <form className="form">
-                    <input className="form__input" placeholder="Co jest do zrobienia?" autofocus/>
-                    <button className="form__button">Dodaj zadanie</button>
-                </form>
+          <h2 className="section__title">Dodaj nowe zadanie</h2>
+          <Form />
         </section>
         <section className="section">
-        <header className="section__header">
-           <h2 className="section__title"> Lista zadań</h2>
-          <div className="stat__button">
-            <button className="stat__button js-toggleHideDoneButton">Ukryj ukończone</button>
-            <button className="stat__button js-markALLDoneButton">Ukończ wszystkie</button> 
-          </div>
-        </header>
-           <ul className="tasks"></ul>
-        </section>
-    </main>
-    </body>
+          <header className="section__header">
+            <h2 className="section__title"> Lista zadań</h2>
 
+            <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
+          </header>
+          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
+        </section>
+      </main>
+    </body>
   );
 }
 
