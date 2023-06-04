f0dd2d6b5d6ace787fc7577adc8596e1e5a3f8d0
Artur Mochnia <amochnia@gmail.com>
2023-06-04 12:14:41 +0100

    delete import React from react

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import React from "react";
 import Form from "./Form";
 import Tasks from "./Tasks";
 import Buttons from "./Buttons";
@@ -16,21 +15,16 @@ const hideDoneTasks = false;
 function App() {
   return (
     <Container>
-        <Header title="Lista zadań" />
-        <Section 
-        title="Dodaj nowe zadanie" 
-        body={<Form />}
-        />
-        <Section 
+      <Header title="Lista zadań" />
+      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section
         title="Lista zadań"
-        body={
-          <Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />
-        }
+        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
         extraHeaderContent={
           <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
         }
-        />
-      </Container>
+      />
+    </Container>
   );
 }
 
