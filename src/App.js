0acaf332f7f06ffaa4e76ab9b40ffdb0022e2229
Artur Mochnia <amochnia@gmail.com>
2023-06-05 22:20:51 +0100

    adding new task interaction

--- src/App.js
+++ src/App.js
@@ -41,10 +41,24 @@ function App() {
     );
   };
 
+  const addNewTask = (content) => {
+    setTasks(tasks => [
+      ...tasks,
+      {
+        content,
+        done: false,
+        id: tasks.lenght ? tasks[tasks.lenght - 1].id + 1 : 1,
+      },
+    ]);
+  };
+
   return (
     <Container>
       <Header title="Lista zadań" />
-      <Section title="Dodaj nowe zadanie" body={<Form />} />
+      <Section
+        title="Dodaj nowe zadanie"
+        body={<Form addNewTask={addNewTask} />}
+      />
       <Section
         title="Lista zadań"
         body={
