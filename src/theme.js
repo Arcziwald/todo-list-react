2b492e8d74d929abc72d34dbe762e33ab3099c9b
Artur Mochnia <amochnia@gmail.com>
2023-07-10 17:16:03 +0100

    added theme.js

---
+++ src/theme.js
@@ -0,0 +1,19 @@
+export const theme = {
+  color: {
+    black: "#000000",
+    white: "#ffffff",
+    alto: "#ddddd",
+    forestGreen: "#228c22",
+    crimson: "#db1414",
+    torchRed: "#fa2020",
+    atoll: "#097285",
+    brightTurquoise: "#12c9e9",
+    tangerine: "#eb9800",
+    gallery: "#ededed",
+    teal: "#008080",
+    persionGreen: "#009999",
+  },
+  breakpoint: {
+    mobileMax: 768,
+  },
+};
